31683019f1e159a188f7373665b1bb8c4dddc246
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-15 12:55:59 +0700

    style: format generated FRB module declaration

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,8 @@
-mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 pub mod api;
 mod codec;
 #[cfg(feature = "encryption")]
 pub mod crypto;
 mod db;
+mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. */
 
 pub use api::*;
