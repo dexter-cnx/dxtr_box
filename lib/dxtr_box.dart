269cde69482b126f72e59f91a1abea3c4bcde355
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-16 13:35:31 +0700

    chore: keep migration internals private

--- lib/dxtr_box.dart
+++ lib/dxtr_box.dart
@@ -1,5 +1,5 @@
 export 'src/box.dart';
 export 'src/box_event.dart';
-export 'src/dxtr_box.dart';
+export 'src/dxtr_box.dart' show DxtrBox;
 export 'src/hive_ce_migration.dart';
 export 'src/query.dart';
